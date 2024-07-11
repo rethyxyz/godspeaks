41d5c3db52884eb2c44d52f56976538a5c2d2244
Brody Rethy <brodyrethy@gmail.com>
2024-07-11 00:55:49 -0300

    cleaner output

--- godspeaks.py
+++ godspeaks.py
@@ -2,7 +2,5 @@ import random
 
 god_words = open("happy.txt", "r").read().splitlines()
 
-print(end="God says... ")
-
-for n in range(0, 30): print(random.choice(god_words), end=" ")
+for n in range(0, 30): print(random.choice(god_words).replace("_", " "), end=" ")
 print("\n")
